f8c250990736a2634d141423454d98ff5fb12139
vijaykasthurik <vijaykasthuri777@gmail.com>
2025-07-27 22:57:47 +0530

    Deploy-ready React build

--- src/signupLogin.jsx
+++ src/signupLogin.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 import { useNavigate} from 'react-router-dom';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://growlify-backend.onrender.com/api';
 
 function SignupLogin({ defaultTab = "signup" }) {
     const navigate = useNavigate();
